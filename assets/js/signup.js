5ae5ee8e9377fb690202b281479e21be9049a95d
Rodrigo Cunha <rodrigocunha@live.com>
2023-10-06 11:38:27 -0300

    signup js created

---
+++ assets/js/signup.js
@@ -0,0 +1,127 @@
+let btn = document.querySelector("#verSenha");
+let btnConfirm = document.querySelector("#verConfirmSenha");
+
+let nome = document.querySelector("#nome");
+let labelNome = document.querySelector("#labelNome");
+let validNome = false;
+
+let usuario = document.querySelector("#usuario");
+let labelUsuario = document.querySelector("#labelUsuario");
+let validUsuario = false;
+
+let senha = document.querySelector("#senha");
+let labelSenha = document.querySelector("#labelSenha");
+let validSenha = false;
+
+let confirmSenha = document.querySelector("#confirmSenha");
+let labelconfirmSenha = document.querySelector("#labelconfirmSenha");
+let validconfirmSenha = false;
+
+let msgError = document.querySelector("#msgError");
+let msgSucess = document.querySelector("#msgError");
+
+nome.addEventListener("keyup", () => {
+  if (nome.value.length <= 2) {
+    labelNome.setAttribute("style", "color: red");
+    labelNome.innerHTML = "Nome *Insira no minimo 3 caracteres";
+    nome.setAttribute("style", "border-color: red");
+    validNome = false;
+  } else {
+    labelNome.setAttribute("style", "color: green");
+    labelNome.innerHTML = "Nome";
+    nome.setAttribute("style", "border-color: green");
+    validNome = true;
+  }
+});
+
+usuario.addEventListener("keyup", () => {
+  if (usuario.value.length <= 4) {
+    labelUsuario.setAttribute("style", "color: red");
+    labelUsuario.innerHTML = "Usuário *Insira no minimo 5 caracteres";
+    usuario.setAttribute("style", "border-color: red");
+    validUsuario = false;
+  } else {
+    labelUsuario.setAttribute("style", "color: green");
+    labelUsuario.innerHTML = "Usuario";
+    usuario.setAttribute("style", "border-color: green");
+    validUsuario = true;
+  }
+});
+
+senha.addEventListener("keyup", () => {
+  if (senha.value.length <= 5) {
+    labelSenha.setAttribute("style", "color: red");
+    labelSenha.innerHTML = "Senha *Insira no minimo 6 caracteres";
+    senha.setAttribute("style", "border-color: red");
+    validSenha = false;
+  } else {
+    labelSenha.setAttribute("style", "color: green");
+    labelSenha.innerHTML = "Senha";
+    senha.setAttribute("style", "border-color: green");
+    validSenha = true;
+  }
+});
+
+confirmSenha.addEventListener("keyup", () => {
+  if (senha.value != confirmSenha.value) {
+    labelconfirmSenha.setAttribute("style", "color: red");
+    labelconfirmSenha.innerHTML = "Confirmar senha *As senhas não conferem";
+    confirmSenha.setAttribute("style", "border-color: red");
+    validconfirmSenha = false;
+  } else {
+    labelconfirmSenha.setAttribute("style", "color: green");
+    labelconfirmSenha.innerHTML = "Confirmar Senha";
+    confirmSenha.setAttribute("style", "border-color: green");
+    validconfirmSenha = true;
+  }
+});
+
+function cadastrar() {
+  if (validNome && validUsuario && validSenha && validconfirmSenha) {
+    let listaUser = JSON.parse(localStorage.getItem("listaUser") || "[]");
+
+    listaUser.push({
+      nomeCad: nome.value,
+      userCad: usuario.value,
+      senhaCad: senha.value,
+    });
+
+    localStorage.setItem("listaUser", JSON.stringify(listaUser));
+
+    msgSuccess.setAttribute("style", "display: block");
+    msgSuccess.innerHTML = "<strong>Cadastrando usuário...</strong>";
+    msgError.setAttribute("style", "display: none");
+    msgError.innerHTML = "";
+
+    setTimeout(() => {
+      window.location.href =
+        "https://cdpn.io/pen/debug/QWzqYPE?authentication_hash=gaMeYggjvGNM";
+    }, 3000);
+  } else {
+    msgError.setAttribute("style", "display: block");
+    msgError.innerHTML =
+      "<strong>Preencha todos os campos antes de cadastrar.</strong>";
+    msgSuccess.innerHTML = "";
+    msgSuccess.setAttribute("style", "display: none");
+  }
+}
+
+btn.addEventListener("click", () => {
+  let inputSenha = document.querySelector("#senha");
+
+  if (inputSenha.getAttribute("type") == "password") {
+    inputSenha.setAttribute("type", "text");
+  } else {
+    inputSenha.setAttribute("type", "password");
+  }
+});
+
+btnConfirm.addEventListener("click", () => {
+  let inputConfirmSenha = document.querySelector("#confirmSenha");
+
+  if (inputConfirmSenha.getAttribute("type") == "password") {
+    inputConfirmSenha.setAttribute("type", "text");
+  } else {
+    inputConfirmSenha.setAttribute("type", "password");
+  }
+});
